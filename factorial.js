8d2565e8105eb61c3987a654d46d806455ded37f
jianlin0 <jianlin0@abc.com>
2020-05-21 16:14:18 +0200

    add calling result

--- factorial.js
+++ factorial.js
@@ -6,3 +6,9 @@ function factorial(number){
         return number * factorial(number -1);
     }
 }
+
+console.log(factorial(1));//1
+console.log(factorial(2));//2
+console.log(factorial(3));//6
+console.log(factorial(4));//24
+console.log(factorial(5));//120
